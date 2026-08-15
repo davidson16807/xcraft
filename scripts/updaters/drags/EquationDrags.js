43c25f1da2f4bcf7d8b7d5264a43fc57510790cb
Carl Davidson <davidson16807@gmail.com>
2026-08-15 11:07:22 -0600

    lone elements can now be dragged either by multiplication or addition

--- scripts/updaters/drags/EquationDrags.js
+++ scripts/updaters/drags/EquationDrags.js
@@ -1,10 +1,10 @@
 'use strict';
-// HUMAN VETTED
 
 function EquationDrags(equations) {
-    function drag_value(source_path, start, current, candidates, target_key) {
+    function drag_value(source_path, source_operation, start, current, candidates, target_key) {
         return Object.freeze({
             source_path: source_path,
+            source_operation: source_operation,
             start: Object.freeze({ x:start.x, y:start.y }),
             current: Object.freeze({ x:current.x, y:current.y }),
             candidates: candidates,
@@ -13,13 +13,13 @@ function EquationDrags(equations) {
     }
 
     return Object.freeze({
-        symbol: function(equation, source_path, start) {
+        symbol: function(equation, source_path, source_operation, start) {
             return Object.freeze({
                 id: DragState.symbol,
-                initialize: () => drag_value(source_path, start, start, equations.moves_for_source(equation, source_path), null),
-                move: (state, point, target_key) => drag_value(source_path, state.start, point, state.candidates, target_key),
+                initialize: () => drag_value(source_path, source_operation, start, start, equations.moves_for_source(equation, source_path, source_operation), null),
+                move: (state, point, target_key) => drag_value(source_path, source_operation, state.start, point, state.candidates, target_key),
                 command: (state, target_key) => equation_input =>
-                    equations.move(equation_input, state.source_path, target_key),
+                    equations.move(equation_input, state.source_path, target_key, state.source_operation),
             });
         },
 
