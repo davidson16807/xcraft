669132eee909057e46a94be0889a4e4708fd1429
Carl Davidson <davidson16807@gmail.com>
2026-08-23 22:57:29 -0600

    introducing EquationDragChoice

---
+++ scripts/models/equation/EquationDragChoice.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class EquationDragChoice {
+    constructor(expression, operator, equation, side, type) {
+        this.expression = expression;
+        this.operator = operator || null;
+        this.equation = equation;
+        this.side = side;
+        this.type = type;
+        Object.freeze(this);
+    }
+}
