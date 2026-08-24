bf68b62cca4e0f3d3aef6e937f208d38b49faff7
Carl Davidson <davidson16807@gmail.com>
2026-08-23 21:33:19 -0600

    introducing drag_choices to AppState

--- scripts/models/app/AppState.js
+++ scripts/models/app/AppState.js
@@ -2,7 +2,7 @@
 // HUMAN VETTED
 
 /*
-`AppState` contains all application state.  Model values are immutable so the
+`AppState` contains all application state. Model values are immutable so the
 undo/redo stacks can store references to old states cheaply and safely.
 */
 class AppState {
@@ -12,6 +12,7 @@ class AppState {
         equation,
         drag_type,
         drag_state,
+        drag_choices,
         undo_history,
         redo_history,
         theme,
@@ -22,6 +23,7 @@ class AppState {
         this.equation = equation;
         this.drag_type = drag_type;
         this.drag_state = drag_state;
+        this.drag_choices = Object.freeze([...(drag_choices || [])]);
         this.undo_history = Object.freeze([...(undo_history || [])]);
         this.redo_history = Object.freeze([...(redo_history || [])]);
         this.theme = theme || 'day';
@@ -38,6 +40,7 @@ class AppState {
             attributes.equation      != null? attributes.equation         : this.equation,
             attributes.drag_type     != null? attributes.drag_type        : this.drag_type,
             attributes.drag_state    != null? attributes.drag_state       : this.drag_state,
+            attributes.drag_choices  != null? attributes.drag_choices     : this.drag_choices,
             attributes.undo_history  != null? attributes.undo_history     : this.undo_history,
             attributes.redo_history  != null? attributes.redo_history     : this.redo_history,
             attributes.theme         != null? attributes.theme            : this.theme,
