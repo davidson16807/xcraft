b5ce9d972be6b4b27966767acb085edd2425d3c5
Carl Davidson <davidson16807@gmail.com>
2026-09-02 00:04:20 -0600

    simplifying ExpressionShape logic to support larger lists of operations

--- scripts/models/expression/ExpressionShape.js
+++ scripts/models/expression/ExpressionShape.js
@@ -4,14 +4,11 @@
 const ExpressionShape = grouplikes => {
     function encode(expression) {
         typecheck(expression, 'Expression+Relation');
-        switch (expression.type) {
-            case 'slot': return `slot`;
-            case 'constant': return `C(${expression.contents})`;
-            case 'variable': return `V(${expression.contents})`;
-            default: {
-                const contents = expression.contents.map(encode);
-                return `${expression.type}(${grouplikes.canonicalize(expression.type, contents).join(',')})`;
-            }
+        if (!Array.isArray(expression.contents)) {
+            return `${expression.type}(${expression.contents})`;
+        } else {
+            const contents = expression.contents.map(encode);
+            return `${expression.type}(${grouplikes.canonicalize(expression.type, contents).join(',')})`;
         }
     }
     return Object.freeze({encode});
